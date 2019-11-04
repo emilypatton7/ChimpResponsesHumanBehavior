14b57d3fd2cdde81d8830a74d3dd386d5da344b2
Clay Arango <ArangoC@cwu.edu>
2019-11-04 14:53:33 -0800

    add manova

--- co-comc.R
+++ co-comc.R
@@ -5,6 +5,7 @@
 #load and process data
 co.comc = read.table(file="CO.COMC.csv", header = T, sep = ",")#load table into R
 str(co.comc)#returns structure of table
+names(co.comc)
 
 #total carry over seconds
 co.comc$total.co = co.comc$CAfCO + co.comc$HAfCO + co.comc$NASCO + co.comc$OtherCO + co.comc$AbCO + co.comc$TrCO + co.comc$InCO
@@ -41,3 +42,35 @@ co.comc$In.d = co.comc$InCO.p - co.comc$InCOMC.p
 
 #graphs
 hist(co.comc$Caf.d)
+
+
+#condense dependent variables
+y <- cbind(co.comc$Caf.d, co.comc$Haf.d, co.comc$Nas.d, co.comc$Other.d, co.comc$Ab.d, co.comc$Tr.d, co.comc$In.d)#combines dependent variables
+
+#run manova
+manova(y ~ Condition * Life, data=co.comc, na.action=na.omit)
+M1 <- manova(y ~ Condition * Life, data=co.comc, na.action=na.omit)
+summary(M1, tol=0)#tol=0 overrides error code, overall test summary
+summary.aov(M1)
+
+#interaction is not significant, so it can be removed
+manova(y ~ Condition + Life, data=co.comc, na.action=na.omit)
+M1 <- manova(y ~ Condition + Life, data=co.comc, na.action=na.omit)
+summary(M1, tol=0)#tol=0 overrides error code, overall test summary
+summary.aov(M1)
+
+#can remove life history since it is not significant
+manova(y ~ Condition, data=co.comc, na.action=na.omit)
+M1 <- manova(y ~ Condition, data=co.comc, na.action=na.omit)
+summary(M1, tol=0)#tol=0 overrides error code, overall test summary
+summary.aov(M1)
+
+#use graphics to analyze residuals
+plot(residuals(M1))
+qqnorm(residuals(M1))#this looks bad
+qqline(residuals(M1))
+hist(residuals(M1))#but this looks really good
+
+#response 6 = Travel is significant, paired t-test to analyze
+t.test(co.comc$Tr.d ~ co.comc$Condition,  alternative = c("two.sided"), paired=F)
+#chimpanzees travelled more in the carry over matched control after chimpanzee interaction
